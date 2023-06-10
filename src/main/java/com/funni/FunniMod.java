58e6eb53556ed25044146476cc0090fd45683566
DerOfenkaese <jannikschulze@gmail.com>
2023-06-10 22:05:32 +0200

    Stupid stick took almost 2 hours

--- src/main/java/com/funni/FunniMod.java
+++ src/main/java/com/funni/FunniMod.java
@@ -1,15 +1,23 @@
-package com.example;
+package com.funni;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class FunniMod implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("funni");
+    
+    public static final Item FUNNI_STICK = new Item(new FabricItemSettings());
 
     @Override
     public void onInitialize() {
@@ -17,6 +25,7 @@ public class ExampleMod implements ModInitializer {
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
 
-        LOGGER.info("Hello Fabric world!");
+        Registry.register(Registries.ITEM, new Identifier("funni", "funni_stick"), FUNNI_STICK);
+
     }
 }

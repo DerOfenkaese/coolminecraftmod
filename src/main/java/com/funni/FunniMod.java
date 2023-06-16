1eeb76337e9505669c7c04b867e68630eb786141
DerOfenkaese <jannikschulze@gmail.com>
2023-06-16 19:44:21 +0200

    add funni stick to combat in creative inventory

--- src/main/java/com/funni/FunniMod.java
+++ src/main/java/com/funni/FunniMod.java
@@ -2,9 +2,12 @@ package com.funni;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvents;
@@ -24,16 +27,22 @@ public class FunniMod implements ModInitializer {
         @Override
         public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
             playerEntity.playSound(SoundEvents.BLOCK_AMETHYST_CLUSTER_BREAK, 1.0F, 1.0F);
+            playerEntity.setAbsorptionAmount(10);
             return TypedActionResult.success(playerEntity.getStackInHand(hand));
         }
     }
     
-    public static final FunniStick FUNNI_STICK = Registry.register(Registries.ITEM, new Identifier("funni", "funni_stick"), new FunniStick(new FabricItemSettings().maxCount(1)));
+    public static final FunniStick FUNNI_STICK = Registry.register(Registries.ITEM, new Identifier("funni", "funni_stick"),
+        new FunniStick(new FabricItemSettings().maxCount(1)));
     
-
+    
     @Override
     public void onInitialize() {
         
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+	    content.addAfter(Items.TURTLE_HELMET, FUNNI_STICK);
+    });
         
     }
+
 }
